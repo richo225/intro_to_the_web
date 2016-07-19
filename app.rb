ade43532b13bed5b3acd591d5abf86672bd1a322
cyberplanner <saleh.gfx@gmail.com>
2016-07-19 14:39:38 +0100

    14 Sinatra: POSTed params

--- app.rb
+++ app.rb
@@ -24,8 +24,14 @@ get "/random-cat" do
   erb(:index)
 end
 
-get "/named-cat" do
+post "/named-cat" do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get "/cat-form" do
+  p params
+  @name = params[:name]
+  erb(:cat_form)
 end
